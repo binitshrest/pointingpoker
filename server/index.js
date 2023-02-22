b9a91c91874401500b507f2dda41eae45e0e4424
abizek <91976421+abizek@users.noreply.github.com>
2023-02-22 21:57:43 +0530

    add standard

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello world')
+  res.send('Hello world')
 })
 
 app.listen(port, () => {
-    console.log(`Running on port ${port}`)
+  console.log(`Running on port ${port}`)
 })
