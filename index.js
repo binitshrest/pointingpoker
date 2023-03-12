a701a0d66d3c023ee8a25f88820d897d4991301d
abizek <91976421+abizek@users.noreply.github.com>
2023-03-12 17:59:57 +0530

    use xo instead of standard

--- index.js
+++ index.js
@@ -1,66 +1,67 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
 class Observable {
-  constructor () {
-    this.observers = new Set()
-  }
+	constructor() {
+		this.observers = new Set();
+	}
 
-  subscribe (func) {
-    this.observers.add(func)
+	subscribe(func) {
+		this.observers.add(func);
 
-    return () => {
-      this.observers.delete(func)
-    }
-  }
+		return () => {
+			this.observers.delete(func);
+		};
+	}
 
-  publish (data) {
-    for (const func of this.observers) {
-      func(data)
-    }
-  }
+	publish(data) {
+		for (const func of this.observers) {
+			func(data);
+		}
+	}
 }
 
-const app = express()
-const observable = new Observable()
+const app = express();
+const observable = new Observable();
 
-app.use(cors())
+app.use(cors());
 
-let count = 0
+let count = 0;
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (request, response) => {
+	response.send("Hello world");
+});
 
-app.post('/count', (req, res) => {
-  count += 1
-  observable.publish(count)
-  res.sendStatus(200)
-})
+app.post("/count", (request, response) => {
+	count += 1;
+	observable.publish(count);
+	response.sendStatus(200);
+});
 
-app.get('/events', (req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    Connection: 'keep-alive',
-    'Cache-Control': 'no-cache'
-  })
+app.get("/events", (request, response) => {
+	response.writeHead(200, {
+		"Content-Type": "text/event-stream",
+		Connection: "keep-alive",
+		"Cache-Control": "no-cache",
+	});
 
-  res.write('retry: 10000\n\n')
+	response.write("retry: 10000\n\n");
 
-  const func = (data) => {
-    res.write(`data: ${data}\n\n`)
-  }
-  func(count)
+	const func = (data) => {
+		response.write(`data: ${data}\n\n`);
+	};
 
-  const unsubscribe = observable.subscribe(func)
+	func(count);
 
-  req.on('close', () => {
-    unsubscribe()
-    console.log('Connection closed')
-  })
-})
+	const unsubscribe = observable.subscribe(func);
 
-const port = 3000
+	request.on("close", () => {
+		unsubscribe();
+		console.log("Connection closed");
+	});
+});
+
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Running on port ${port}`)
-})
+	console.log(`Running on port ${port}`);
+});
