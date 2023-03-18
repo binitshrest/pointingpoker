afda5a97f7bcae7b5dc579962e459a0252407fa5
abizek <91976421+abizek@users.noreply.github.com>
2023-03-18 11:52:49 +0530

    refactor store subscribe

--- client/src/store.js
+++ client/src/store.js
@@ -3,20 +3,19 @@ import { MY_NAME } from "./utils.js";
 
 let data = { votes: {} };
 
+const eventSource = new EventSource(
+	`${import.meta.env.VITE_SERVER_BASE_URL}/events/${MY_NAME}`
+);
+
+eventSource.addEventListener("message", (event) => {
+	data = JSON.parse(event.data);
+	console.log("new data", data);
+});
+
 function subscribe(callback) {
-	const eventSource = new EventSource(
-		`${import.meta.env.VITE_SERVER_BASE_URL}/events/${MY_NAME}`
-	);
+	eventSource.addEventListener("message", callback);
 
-	const listener = (event) => {
-		data = JSON.parse(event.data);
-		console.log("new data", data);
-		callback();
-	};
-
-	eventSource.addEventListener("message", listener);
-
-	return () => eventSource.removeEventListener("message", listener);
+	return () => eventSource.removeEventListener("message", callback);
 }
 
 function getSnapshot() {
