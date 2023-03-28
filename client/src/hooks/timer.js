04e9780fb590643c0f6ee80c1ac5557feaae1f5b
abizek <91976421+abizek@users.noreply.github.com>
2023-03-28 08:23:02 +0530

    add hour in timer

--- client/src/hooks/timer.js
+++ client/src/hooks/timer.js
@@ -2,15 +2,16 @@ import { useEffect, useState } from "react";
 import { DateTime } from "luxon";
 
 export function useTimer(startTime) {
-	const [timer, setTimer] = useState("00:00");
+	const [timer, setTimer] = useState("00:00:00");
 
 	useEffect(() => {
 		const intervalId = setInterval(() => {
 			setTimer(
-				DateTime.now()
-					.setZone("UTC")
-					.minus(startTime)
-					.toLocaleString({ minute: "numeric", second: "2-digit" })
+				DateTime.now().setZone("UTC").minus(startTime).toLocaleString({
+					hour: "2-digit",
+					minute: "numeric",
+					second: "2-digit",
+				})
 			);
 		}, 1000);
 
