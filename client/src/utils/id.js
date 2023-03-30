24e10d8ab671b54edc3c929c8b77a924bf583b64
abizek <91976421+abizek@users.noreply.github.com>
2023-03-30 19:46:14 +0530

    use new id each time

--- client/src/utils/id.js
+++ client/src/utils/id.js
@@ -1,9 +1,3 @@
 import { nanoid } from "nanoid";
 
-function createId() {
-	const createdId = nanoid();
-	localStorage.setItem("id", createdId);
-	return createdId;
-}
-
-export const id = localStorage.getItem("id") ?? createId();
+export const id = nanoid();
