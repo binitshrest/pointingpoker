12f54092e5a2896d0ec18c139ff58ddde090495a
abizek <91976421+abizek@users.noreply.github.com>
2023-08-05 23:54:34 +0530

    fix default room

--- src/utils/room-id.js
+++ src/utils/room-id.js
@@ -1,7 +1,7 @@
-const roomId = location.hash.split("#")[1];
-
-if (!roomId) {
-  location.hash = "default";
+if (!location.hash.split("#")[1]) {
+  location.hash = "default-room";
 }
+
+const roomId = location.hash.split("#")[1];
 
 export { roomId };
